e1dea251c240181e7eeb36561ed16ef2f0a79803
Nacho <nacho.cassi97@gmail.com>
2021-09-06 23:53:28 -0300

    Added 10 excercices

--- src/App.java
+++ src/App.java
@@ -1,6 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        //Excercices.JavaCurrencyFormatter();
-        Excercices.JavaStringTokens();
+        runHourglassSum();
     }
+
+    public static void runHourglassSum() {
+        List<List<Integer>> arr = new ArrayList<>();
+        for (int i=0; i<=5; i++) {
+            arr.add(new ArrayList<Integer>());
+        }
+
+        for (int i=0; i<=5; i++) {
+            for (int j=0; j<=5; j++) {
+                arr.get(i).add(1);
+            }
+        }
+        System.out.println(arr);
+        System.out.println(Excercices.hourglassSum(arr));
+        
+    }
+
 }
--- src/Excercices.java
+++ src/Excercices.java
@@ -1,8 +1,8 @@
 import java.text.NumberFormat;
 import java.util.Scanner;
-
-import javax.swing.plaf.synth.SynthStyleFactory;
-
+import java.util.ArrayList;
+//import java.util.regex.*;
+import java.util.List;
 import java.util.Locale;
 
 public class Excercices {
@@ -43,5 +43,97 @@ public class Excercices {
         } else {
             System.out.println(s.length());
         }
+    }
+
+    public static void JavaRegex() {
+        class pattern {
+            String zeroTo255 = "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
+            String pattern = zeroTo255+"\\."+zeroTo255+"\\."+zeroTo255+"\\."+zeroTo255;
+        }
+    }
+
+    public static int countingValleys(int steps, String path) {
+        int valleys = 0;
+        int currentHeight = 0;
+        boolean inValley = false;
+        for (int stepNumber=0; stepNumber<steps; stepNumber++) {
+            Character stepDirection = path.charAt(stepNumber);
+            if (stepDirection.charValue()=='D' && currentHeight==0) {
+                currentHeight--;
+                inValley = true;
+            } else if (stepDirection.charValue()=='D') currentHeight--;
+            if (stepDirection.charValue()=='U' && currentHeight==-1) {
+                currentHeight++;
+                inValley = false;
+                valleys++;
+            } else if (stepDirection.charValue()=='U') currentHeight++;
+        }
+        return valleys;
+    }
+
+    public static int countingValleysv2(int steps, String path) {
+        int valleys = 0;
+        int currentHeight = 0;
+        char[] paths = path.toCharArray();
+        for (char direction : paths) {
+            if (direction=='D') currentHeight--;
+            if (direction=='U') currentHeight++;
+            if (currentHeight==0 && direction=='U') valleys++;
+        }
+        return valleys;
+    }
+
+    public static Integer jumpingOnClouds(List<Integer> c) {
+        Integer totalJumps = 0;
+        Integer jump = 0;
+        while (jump!=c.size()-1) {
+            if (jump+2 <= c.size()-1) {
+                if (c.get(jump+2)==0) {
+                    jump = jump+2;
+                } else {
+                    jump = jump+1;
+                }
+                totalJumps++;
+            } else {
+                jump = jump+1;
+                totalJumps++;
+            }
+        }
+        return totalJumps;
+    }
+
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        List<Integer> scores = new ArrayList<>();
+        scores.add(0);
+        scores.add(0);
+        for (int i=0; i<=a.size()-1; i++) {
+            if (a.get(i)>b.get(i)) {
+                scores.set(0,scores.get(0)+1);
+            } else if (a.get(i)<b.get(i)) {
+                scores.set(1,scores.get(1)+1);
+            }
+        }
+        return scores;
+    }
+
+    public static List<Integer> reverseArray(List<Integer> array) {
+        List <Integer> reversedArray = new ArrayList<>();
+        for (int i = array.size()-1; i>=0; i--) {
+            reversedArray.add(array.get(i));
+        }
+        return reversedArray;
+    }
+
+    public static int hourglassSum(List<List<Integer>> arr) {
+        int largestSum = 0;
+        int totalHgs = 0;
+        int i=0;
+        int j=0;
+        while (i<=3) {
+            while (j<=3) {
+                continue;
+            }
+        }
+        return largestSum;
     }
 }
